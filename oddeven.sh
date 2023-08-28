1c4be46bd73c638d022e0536619f26ba126d3777
Prasad Mache <89181027+prasad-mache@users.noreply.github.com>
2023-08-28 10:22:23 +0530

    Update oddeven.sh

--- oddeven.sh
+++ oddeven.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+read -p "Enter the number and I will check if it is Even or Odd" mynumber
+
+if [ $((mynumber%2)) -eq 0 ]
+then 
+echo "$mynumber is Even"
+else
+echo "$mynumber is odd"
+fi
